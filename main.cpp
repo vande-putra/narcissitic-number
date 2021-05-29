a2d26f8babacb749685449b3dc8d63dea1948b23
VandePutra <50024222+vande-putra@users.noreply.github.com>
2021-05-29 18:02:37 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+bool narcissistic(int n);
+int main() {
+  std::cout << narcissistic(371) << std::endl;
+  std::cout << narcissistic(122) << std::endl;
+  std::cout << narcissistic(4887) << std::endl;
+  return 0;
+}
+bool narcissistic(int n){
+  int Num, a, r;
+  r = 0;
+  Num = n;
+
+  while (Num != 0) {
+        a = Num % 10;
+        
+        r += a * a * a;
+        Num = Num / 10;
+    }
+
+    if (r == value)
+        return true;
+    else
+        return false;
+}
